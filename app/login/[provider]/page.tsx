420b471e29d430ab1d23c75872c6cf0469f4324e
DaegyunOh <oooppq@naver.com>
2024-01-01 19:06:25 +0900

    feat: 카카오 로그인 기능 구현

--- app/login/[provider]/page.tsx
+++ app/login/[provider]/page.tsx
@@ -1,5 +1,25 @@
-const page = () => {
-  return <div>page</div>;
+import { redirect } from 'next/navigation';
+
+interface RedirectPageProps {
+  params: { provider: string };
+  searchParams: { code: string };
+}
+
+const page = async ({ params, searchParams }: RedirectPageProps) => {
+  if (!searchParams || !searchParams.code) redirect('/login');
+
+  const res = await fetch(
+    `${process.env.SERVER_URL}/login?code=${searchParams.code}&provider=${params.provider}`,
+    { method: 'POST' },
+  );
+  if (res.ok) {
+    const userInfo = await res.json();
+    console.log(userInfo);
+
+    redirect('/');
+  } else {
+    redirect('/login');
+  }
 };
 
 export default page;
