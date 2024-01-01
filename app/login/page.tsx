ffb9d03b4bc90b740d792c7fa49d7fe7d76346fa
DaegyunOh <oooppq@naver.com>
2024-01-01 17:18:31 +0900

    style: 로그인 페이지 기본 디자인 적용

---
+++ app/login/page.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+const page = () => {
+  return (
+    <div className="flex flex-col items-center px-4 w-full h-full">
+      <h1 className="mb-auto text-xl font-semibold">향수에 입문하기</h1>
+      <Link
+        href="/"
+        className="w-full bg-[#FEE500] mb-2.5 rounded-lg h-[52px] inline-flex items-center justify-center relative"
+      >
+        <p className="">카카오 로그인</p>
+      </Link>
+      <Link
+        href="/"
+        className="w-full bg-black text-white rounded-lg h-[52px] inline-flex items-center justify-center relative"
+      >
+        둘러볼래요?
+      </Link>
+    </div>
+  );
+};
+
+export default page;
