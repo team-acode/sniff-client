ea11a11b2ca71afb3b4d68bdffb136daa2e80163
DaegyunOh <oooppq@naver.com>
2024-01-03 01:10:48 +0900

    style: 홈화면 가운데 메뉴 디자인 적용 완료

--- components/home/Menu.tsx
+++ components/home/Menu.tsx
@@ -1,5 +1,30 @@
+import { AcodeLogoMono } from '@/public/images';
+import Link from 'next/link';
+
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div className="mt-9 ml-4 flex text-[14px]">
+      <Link
+        href="/"
+        className="w-[198px] h-[95px] pt-[17px] pl-[16px] pr-[18px] font-medium text-acodeblack bg-acodegray-50 rounded-l-lg leading-[21px] flex justify-between"
+      >
+        <span className="">
+          내 취향 향수
+          <br />
+          확인하기
+        </span>
+
+        <AcodeLogoMono className="pt-[5px]" />
+      </Link>
+      <Link
+        href="/"
+        className="w-[143px] h-[95px] ml-4 pt-[17px] pl-[15px] text-acodegray-700 bg-acodegray-50 rounded-lg flex flex-col"
+      >
+        <span className="mb-auto">큐레이션 / 광고</span>
+        <span className="text-[12px] mb-[11px]">{'더보기 >'}</span>
+      </Link>
+    </div>
+  );
 };
 
 export default Menu;
--- public/images/index.ts
+++ public/images/index.ts
@@ -1,8 +1,10 @@
 export { default as AcodeLogoBig } from './acode-logo-big.svg';
+export { default as AcodeLogoSmall } from './acode-logo-small.svg';
+export { default as AcodeLogoMono } from './acode-logo-mono.svg';
+
 export { default as KakaoLogo } from './kakao-logo.svg';
 export { default as Onboarding } from './onboarding.svg';
 export { default as UserIcon } from './user-icon.svg';
 export { default as SearchIcon } from './search-icon.svg';
 export { default as MenuIcon } from './menu-icon.svg';
-export { default as AcodeLogoSmall } from './acode-logo-small.svg';
 export { default as ArrowRightIcon } from './arrow-right-icon.svg';
