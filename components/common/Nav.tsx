7e77a193f49996918cdf775d30bc9daee672c728
DaegyunOh <oooppq@naver.com>
2024-01-03 00:23:35 +0900

    style: 임시 nav bar 스타일 적용

---
+++ components/common/Nav.tsx
@@ -0,0 +1,20 @@
+import {
+  AcodeLogoSmall,
+  MenuIcon,
+  SearchIcon,
+  UserIcon,
+} from '@/public/images';
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div className="flex mt-[3px] ml-[10px] mr-[13px] relative">
+      <MenuIcon className="mt-[6px] mr-auto" />
+      <AcodeLogoSmall className="absolute top-[7px] left-1/2 -translate-x-2/4" />
+      <SearchIcon className="mt-[3px] mr-[11px]" />
+      <UserIcon className="mt-[3px]" />
+    </div>
+  );
+};
+
+export default Nav;
--- public/images/index.ts
+++ public/images/index.ts
@@ -1,3 +1,7 @@
 export { default as AcodeLogoBig } from './acode-logo-big.svg';
 export { default as KakaoLogo } from './kakao-logo.svg';
 export { default as Onboarding } from './onboarding.svg';
+export { default as UserIcon } from './user-icon.svg';
+export { default as SearchIcon } from './search-icon.svg';
+export { default as MenuIcon } from './menu-icon.svg';
+export { default as AcodeLogoSmall } from './acode-logo-small.svg';
