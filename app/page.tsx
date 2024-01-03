fc2128237d86cc8dac35ce5c787678ec62b722c8
DaegyunOh <oooppq@naver.com>
2024-01-03 15:48:41 +0900

    style: 더보러가기 부분 스타일 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Menu from '@/components/home/Menu';
 import PerfumeList from '@/components/home/PerfumeList';
 import SpiceRecommendation from '@/components/home/SpiceRecommendation';
+import { ArrowRightIcon } from '@/public/images';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -8,6 +10,21 @@ export default function Home() {
       <SpiceRecommendation />
       <Menu />
       <PerfumeList />
+      <div className="w-full mt-[31px] pl-4">
+        <h3 className="ml-[10.5px] text-[16px] font-semibold tracking-[-0.4px] text-acodegray-500">
+          <span className="text-acodeblack mr-[5px]">우디</span>
+          계열의 향수
+        </h3>
+        <Link
+          href=""
+          className="px-3 mt-3.5 w-[125px] h-9 pt-1.5 bg-acodeblack rounded-lg text-acodewhite flex items-center"
+        >
+          <span className="tracking-[-0.4px] mr-1 font-semibold shrink-0">
+            더 보러 가기
+          </span>
+          <ArrowRightIcon />
+        </Link>
+      </div>
     </div>
   );
 }
