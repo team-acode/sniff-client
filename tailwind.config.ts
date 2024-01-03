5a81353c28a0bb16280f886446030b864d715dc6
DaegyunOh <oooppq@naver.com>
2024-01-03 16:15:56 +0900

    style: text 디자인 시스템 정해진 것 반영

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,11 +26,6 @@ const config: Config = {
         acodepoint: '#e31243',
         acodeerror: '#ff114a',
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   plugins: [],
