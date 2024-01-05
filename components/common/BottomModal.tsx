4af07d85d8061e3c288703256b1fc5ae073960cb
DaegyunOh <oooppq@naver.com>
2024-01-05 22:42:26 +0900

    feat: 공통된 스타일의 모달을 다루는 컴포넌트 구성

---
+++ components/common/BottomModal.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+
+interface BottomModalProps {
+  children: React.ReactNode;
+  closeModal: () => void;
+  modalStyle: string;
+}
+
+const BottomModal = ({
+  children,
+  closeModal,
+  modalStyle,
+}: BottomModalProps) => {
+  const modalOutsideRef = useRef<HTMLDivElement>(null);
+  const modalRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    document.body.setAttribute('style', 'overflow: hidden');
+
+    return () => document.body.setAttribute('style', 'overflow: auto');
+  }, []);
+
+  return (
+    <div
+      className="fixed left-0 right-0 top-0 bottom-0 z-50 flex flex-col"
+      ref={modalOutsideRef}
+      onClick={(e) => {
+        if (e.target !== modalRef.current) closeModal();
+      }}
+      aria-hidden
+    >
+      <div
+        className={`${modalStyle} bg-acodewhite h-full w-full mt-auto`}
+        ref={modalRef}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default BottomModal;
