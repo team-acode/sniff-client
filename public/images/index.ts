8a0be9753fbdd5a4698ee3e8ab7c8dab44ee723c
Jiseok <chae0_@naver.com>
2024-01-05 16:40:42 +0900

    style: 필요 아이콘추가

--- public/images/index.ts
+++ public/images/index.ts
@@ -11,3 +11,16 @@ export { default as MenuIcon } from './menu-icon.svg';
 export { default as ArrowRightIcon } from './arrow-right-icon.svg';
 export { default as ArrowUpIcon } from './arrow-up-icon.svg';
 export { default as ArrowDownIcon } from './arrow-down-icon.svg';
+
+export { default as EasyOnIcon } from './easy-on-icon.svg';
+export { default as EasyOffIcon } from './easy-off-icon.svg';
+
+export { default as RedFullStar } from './full-star.svg';
+export { default as RedHalfStar } from './half-star.svg';
+export { default as RedEmptyStar } from './empty-star.svg';
+
+export { default as BlackFullStar } from './black-full-star.svg';
+export { default as BlackEmptyStar } from './black-empty-star.svg';
+
+export { default as NoReview } from './no-review-regist.svg';
+export { default as ArrowRightReviewIcon } from './arrow-right-review-icon.svg';
