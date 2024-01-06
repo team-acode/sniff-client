1972cc0083137c2e5fd63a10d7d35a46abe389ad
DaegyunOh <oooppq@naver.com>
2024-01-07 02:06:22 +0900

    chore: 하단 모달 속성 관련 디버깅
    fixed로 하면 max width 적용이 안되기 때문에 absolute으로 변경했고,
    바보같이 height 속성을 중복 입력해서 배포환경에서 모달이 하단에 고정되지 않는 문제가 있어
    이부분을 수정했다.

--- components/common/BottomModal.tsx
+++ components/common/BottomModal.tsx
@@ -23,17 +23,14 @@ const BottomModal = ({
 
   return (
     <div
-      className="fixed left-0 right-0 top-0 bottom-0 z-50 flex flex-col bg-[#1c1818]/[0.7] justify-end"
+      className="absolute left-0 right-0 top-0 bottom-0 z-50 flex flex-col bg-[#1c1818]/[0.7] justify-end"
       ref={modalOutsideRef}
       onClick={(e) => {
         if (e.target === modalOutsideRef.current) closeModal();
       }}
       aria-hidden
     >
-      <div
-        className={`${modalStyle} bg-acodewhite h-full w-full`}
-        ref={modalRef}
-      >
+      <div className={`${modalStyle} bg-acodewhite w-full`} ref={modalRef}>
         {children}
       </div>
     </div>
