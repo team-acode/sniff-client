ea3e9f62a2ced6de18c6fefee68dfb0089685850
DaegyunOh <oooppq@naver.com>
2024-01-07 21:38:39 +0900

    feat: 검색 api 호출 이외의 동작 구현

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,21 +1,45 @@
-import { ArrowLeftIcon, SearchIcon, XCircleIcon } from '@/public/images';
+'use client';
 
-const page = () => {
+import { ArrowLeftIcon, SearchIcon, XCircleIcon } from '@/public/images';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { ChangeEvent, useEffect, useState } from 'react';
+
+const SearchPage = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [query, setQuery] = useState<string>('');
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    router.push(`/search?q=${query}`);
+  };
+
+  useEffect(() => {}, [searchParams]);
+
   return (
     <div className="flex items-center ml-[10px] mr-[16px] mt-[1px]">
-      <ArrowLeftIcon className="fill-acodeblack mr-1" />
+      <button type="button" className="mr-1" onClick={() => router.back()}>
+        <ArrowLeftIcon className="fill-acodeblack" />
+      </button>
       <form
         action=""
         className="flex bg-acodegray-50 flex-1 h-[34px] pl-4 pr-[11px] items-center"
+        onSubmit={handleSubmit}
       >
         <input
           type="text"
           className="body2 text-acodeblack font-medium flex-1 bg-transparent placeholder:text-acodegray-300"
           placeholder="검색어를 입력하세요"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setQuery(e.target.value)
+          }
+          value={query}
         />
-        <button type="button" className="mr-2">
-          <XCircleIcon />
-        </button>
+        {query !== '' ? (
+          <button type="button" className="mr-2" onClick={() => setQuery('')}>
+            <XCircleIcon />
+          </button>
+        ) : null}
         <button type="submit" className="">
           <SearchIcon className="fill-acodeblack" />
         </button>
@@ -24,4 +48,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default SearchPage;
