83e54fc99a540d159abb5fcf6ceedaaed30fa7cf
Jiseok <chae0_@naver.com>
2024-01-14 15:25:30 +0900

    feature : 어코드식 설명 토글버튼 구현

---
+++ components/detail/AcodeExplainButton.tsx
@@ -0,0 +1,47 @@
+'use client';
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { EasyOffIcon, EasyOnIcon } from '@/public/images';
+
+interface AcodeExplainProps {
+  searchParams: { [key: string]: string | undefined };
+  id: number;
+}
+
+const AcodeExplain = ({ searchParams, id }: AcodeExplainProps) => {
+  const [selectedTab, setSelectedTab] = useState(
+    searchParams.selectedTab || 'on',
+  );
+
+  const handleClick = () => {
+    setSelectedTab((prevTab) => {
+      const newTab = prevTab === 'on' ? 'off' : 'on';
+      return newTab;
+    });
+  };
+
+  const updatedSearchParams = {
+    ...searchParams,
+    category: 'detail',
+    easy: selectedTab === 'on' ? 'off' : 'on',
+  };
+
+  return (
+    <div>
+      <div className="flex">
+        <Link
+          href={{
+            pathname: `/perfumes/${id}`,
+            query: updatedSearchParams,
+          }}
+          onClick={handleClick}
+          scroll={false}
+        >
+          <span>{selectedTab === 'on' ? <EasyOnIcon /> : <EasyOffIcon />}</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default AcodeExplain;
