ba4da8451c19001b57869b21bc7548b1024c8f48
oooppq <daegyun.oh@lge.com>
2024-01-17 22:07:16 +0900

    style: 홈화면 디자인 변경사항 적용

--- components/home/SpiceRecommendation.tsx
+++ components/home/SpiceRecommendation.tsx
@@ -14,7 +14,7 @@ const SpiceRecommendation = async () => {
         오늘의 추천 향료는 <br />
         <Link
           href={`spices/${encodeURIComponent(spiceInfo.ingredientName)}`}
-          className="text-white h-11 mt-1.5 px-2.5 py-1 bg-gradient-to-r from-[#705d5d] to-[#a08383]/[.7] rounded-lg justify-center items-center gap-2.5 inline-flex"
+          className="text-white h-11 mt-1.5 px-2.5 py-1 bg-gradient-to-r from-[#705d5d] to-[#a08383]/[.7] rounded-[4px] justify-center items-center gap-2.5 inline-flex"
         >
           <span className="">{spiceInfo.acode}</span>
           <ArrowRightIcon />
