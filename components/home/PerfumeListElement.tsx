d0f3a39a84e89cb373c4a81fd83e6fc255671d7a
오대균/연구원/TV System Core팀(daegyun.oh@lge.com) <daegyun.oh@lge.com>
2024-01-17 21:41:25 +0900

    feat: 카테고리별 추천 향수 api 연결
    수정사항이 남아있어 후추 수정 예정'

--- components/home/PerfumeListElement.tsx
+++ components/home/PerfumeListElement.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 
 interface PerfumeListElementProps {
   perfume: {
-    id: number;
-    name: string;
-    brand: string;
-    tags: string[];
-    backgroundUrl?: string;
+    fragranceId: number;
+    fragranceName: string;
+    brandName: string;
+    style: string[];
+    poster: string;
   };
 }
 
@@ -15,7 +15,7 @@ const PerfumeListElement = ({ perfume }: PerfumeListElementProps) => {
   return (
     <li className="w-full relative mb-[5px]">
       <Image
-        src="/images/popular-perfume-temp-bg.png" // 임시로 지정해둠 변경해야됨
+        src={perfume.poster} // 임시로 지정해둠 변경해야됨
         fill
         style={{ objectFit: 'cover', zIndex: -1 }}
         alt="perfume pg"
@@ -26,19 +26,19 @@ const PerfumeListElement = ({ perfume }: PerfumeListElementProps) => {
         className="w-full pt-[27px] px-[26px] text-left h-60 bg-gradient-to-b from-black/80 to-[#292323]/20 inline-flex flex-col"
       >
         <div className="flex">
-          <h2 className="h0 text-acodewhite mr-2">{perfume.name}</h2>
+          <h2 className="h0 text-acodewhite mr-2">{perfume.fragranceName}</h2>
           <span className="body2 font-medium pt-1.5 text-acodegray-300">
-            {perfume.brand}
+            {perfume.brandName}
           </span>
         </div>
-        <ul className="flex mt-2">
-          {perfume.tags.map((tag) => (
+        {/* <ul className="flex mt-2">
+          {perfume.style.map((tag) => (
             <li className="body2 mr-[9px] font-medium text-acodewhite">
               <span className="text-acodegray-300 mr-[1px]">#</span>
               {tag}
             </li>
           ))}
-        </ul>
+        </ul> */}
       </Link>
     </li>
   );
