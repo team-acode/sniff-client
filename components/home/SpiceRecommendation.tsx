949a2f87242ab2feea0e144cea4d3df75043dae1
오대균/연구원/TV System Core팀(daegyun.oh@lge.com) <daegyun.oh@lge.com>
2024-01-17 21:16:37 +0900

    feat: 오늘의 추천향료 api 연결

--- components/home/SpiceRecommendation.tsx
+++ components/home/SpiceRecommendation.tsx
@@ -1,16 +1,22 @@
 import { ArrowRightIcon } from '@/public/images';
 import Link from 'next/link';
 
-const SpiceRecommendation = () => {
+const SpiceRecommendation = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/home/recommend`,
+  );
+  if (!res.ok) return null;
+  const spiceInfo = await res.json();
+
   return (
     <div className="mt-[43px] ml-4">
       <h1 className="h0 text-acodeblack">
         오늘의 추천 향료는 <br />
         <Link
-          href="/"
+          href={`spices/${encodeURIComponent(spiceInfo.ingredientName)}`}
           className="text-white h-11 mt-1.5 px-2.5 py-1 bg-gradient-to-r from-[#705d5d] to-[#a08383]/[.7] rounded-lg justify-center items-center gap-2.5 inline-flex"
         >
-          <span className="">부드러운 가죽의 향</span>
+          <span className="">{spiceInfo.acode}</span>
           <ArrowRightIcon />
         </Link>
       </h1>
