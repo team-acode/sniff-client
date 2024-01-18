08beec0cb0199a21547db88c5f2722fb279138c7
oooppq <daegyun.oh@lge.com>
2024-01-18 18:51:30 +0900

    hotfix: 홈 nav bar z-index 수정

--- components/home/HomeNav.tsx
+++ components/home/HomeNav.tsx
@@ -15,7 +15,7 @@ const HomeNav = () => {
 
   return (
     <>
-      <div className="fixed top-0 flex items-center w-full max-w-[430px] h-[54px] pt-[13px] pl-[10px] pr-[13px] pb-[11px] bg-acodewhite z-20">
+      <div className="fixed top-0 flex items-center w-full max-w-[430px] h-[54px] pt-[13px] pl-[10px] pr-[13px] pb-[11px] bg-acodewhite z-30">
         <button
           type="button"
           className="mr-auto"
