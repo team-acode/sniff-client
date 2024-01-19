e63cd1876931743d1d71150722aab9dceb4ecc62
Jiseok <chae0_@naver.com>
2024-01-19 18:23:49 +0900

    feat: nextconfig image domain추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['acode-bucket-fragrance.s3.ap-northeast-2.amazonaws.com'],
+    domains: ['acode-bucket-fragrance.s3.ap-northeast-2.amazonaws.com','acode-bucket-review.s3.ap-northeast-2.amazonaws.com'],
   },
   webpack: (config) => {
     config.module.rules.push({
