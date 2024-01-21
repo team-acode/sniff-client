5a25a510640096a02fc0144d1ebe42cdb82ca4d5
DaegyunOh <oooppq@naver.com>
2024-01-21 15:56:26 +0900

    feat: 검색어 query string 상태에 맞게 바뀌도록 수정

--- components/search/SearchBar.tsx
+++ components/search/SearchBar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SearchIcon, XCircleIcon, ArrowLeftIcon } from '@/public/images';
 
 interface SearchBarProps {
@@ -11,6 +11,10 @@ interface SearchBarProps {
 const SearchBar = ({ initialQuery }: SearchBarProps) => {
   const router = useRouter();
   const [query, setQuery] = useState<string>(initialQuery);
+
+  useEffect(() => {
+    if (initialQuery) setQuery(initialQuery);
+  }, [initialQuery]);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -22,18 +26,18 @@ const SearchBar = ({ initialQuery }: SearchBarProps) => {
   };
 
   return (
-    <div className="flex items-center ml-[10px] mr-[21px] mt-[13px] mb-4">
+    <div className="flex items-center h-10 ml-[10px] mr-[21px] mt-[7px] mb-4">
       <button type="button" className="mr-1" onClick={() => router.back()}>
         <ArrowLeftIcon className="fill-acodeblack" />
       </button>
       <form
         action=""
-        className="flex bg-acodegray-50 flex-1 h-[34px] px-[11px] items-center"
+        className="flex bg-acodegray-50 flex-1 h-10 px-[11px] items-center"
         onSubmit={handleSubmit}
       >
         <input
           type="text"
-          className="body2 text-acodeblack font-medium flex-1 bg-transparent placeholder:text-acodegray-300"
+          className="body2 text-acodeblack font-medium h-10 flex-1 bg-transparent placeholder:text-acodegray-300"
           placeholder="검색어를 입력하세요"
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
             setQuery(e.target.value)
