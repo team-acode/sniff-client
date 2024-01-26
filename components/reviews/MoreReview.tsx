ae76e0fa6eabbfde738976533b067de00a059797
DaegyunOh <oooppq@naver.com>
2024-01-26 17:16:15 +0900

    refactor: 리뷰 리스트 코드 정리

--- components/reviews/MoreReview.tsx
+++ components/reviews/MoreReview.tsx
@@ -18,21 +18,6 @@ interface PerfumeReviewProps {
   id: string;
 }
 
-interface Review {
-  reviewId: number;
-  thumbnail: string;
-  image1: string;
-  image2: string;
-  comment: string;
-  rate: number;
-  nickname: string;
-  season: string;
-  intensity: string;
-  longevity: string;
-  style: string;
-  textReview: string;
-}
-
 export async function getReviewList(params: { id: string }) {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_SERVER_URL}/fragrance/${params.id}/review`,
@@ -47,10 +32,25 @@ export async function getReviewList(params: { id: string }) {
   return data;
 }
 
+const keyMapping: { [key: string]: string } = {
+  계절감: 'season',
+  '향의 세기': 'intensity',
+  지속성: 'longevity',
+};
+
+const engToKor = (key: string, value: string) => {
+  if (key === 'season') {
+    return seasonMapping[value];
+  }
+  if (key === 'intensity') {
+    return intensityMapping[value];
+  }
+  return longevityMapping[value];
+};
+
 const MoreReview = async ({ id }: PerfumeReviewProps) => {
   const data = await getReviewList({ id });
   const averageRating = data.rateSum / data.reviewCnt;
-
   const fullStars = Math.floor(averageRating);
   const halfStar = averageRating - fullStars >= 0.5 ? 1 : 0;
   const emptyStars = 5 - fullStars - halfStar;
@@ -72,91 +72,100 @@ const MoreReview = async ({ id }: PerfumeReviewProps) => {
         </div>
       </div>
 
-      {data.reviewInfoList.map((review: Review, index: number) => {
-        const photos = [review.thumbnail, review.image1, review.image2].filter(
-          (url) => url,
-        );
-        const displayNickname =
-          review.nickname.slice(0, 2) + '*'.repeat(review.nickname.length - 2);
-        return (
-          <div key={review.reviewId} className="body2 flex flex-col">
-            <div className="flex flex-row">
-              {photos.map((photo, photoIndex) => (
-                <div key={photo} className="item-center mr-1 mb-5">
-                  <div className="relative w-[115px] h-[115px] overflow-hidden rounded-md">
-                    <Image
-                      src={photo}
-                      alt={`Review Image ${photoIndex}`}
-                      className="rounded-md"
-                      fill
-                    />
-                  </div>
-                </div>
-              ))}
-            </div>
-            <div className="justify-between flex flex-row">
-              <div className="flex flex-col">
-                <div className="flex flex-row items-center text-center mb-5">
-                  <div className="flex ">
-                    {Array.from({ length: review.rate }, (_, i) => (
-                      <BlackFullStar key={`full-${i}`} />
-                    ))}
-                    {Array.from({ length: 5 - review.rate }, (_, i) => (
-                      <BlackEmptyStar key={`empty-${i}`} />
-                    ))}
-                  </div>
+      {data.reviewInfoList.map(
+        (review: { [key: string]: any }, index: number) => {
+          const photos = [
+            review.thumbnail,
+            review.image1,
+            review.image2,
+          ].filter((url) => url);
 
-                  <div className="text-acodegray-400 caption2 ml-3">
-                    {displayNickname}
+          const maskingNickname = (value: string) => {
+            if (value.length === 2) {
+              return value.replace(/(?<=.{1})./gi, '*');
+            }
+            if (value.length > 2) {
+              return value.replace(/(?<=.{2})./gi, '*');
+            }
+            return '*';
+          };
+
+          return (
+            <div key={review.reviewId} className="body2 flex flex-col">
+              <div className="flex flex-row">
+                {photos.map((photo, photoIndex) => (
+                  <div key={photo} className="item-center mr-1 mb-5">
+                    <div className="relative w-[115px] h-[115px] overflow-hidden rounded-[4px]">
+                      <Image
+                        src={photo}
+                        alt={`Review Image ${photoIndex}`}
+                        className="rounded-md"
+                        fill
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="text-[16px] font-semibold leading-[18px] tracking-[-0.4px] text-acodeblack mb-2.5">
-                  {review.comment}
-                </div>
-                <div className="mb-5">{review.textReview}</div>
-                <div className="space-y-4 font-medium">
-                  <div className="flex flex-row gap-y-[42px]">
-                    <div className="flex w-12 text-acodegray-500">계절감</div>
-                    <div className="flex w-16">
-                      {seasonMapping[review.season]}
+                ))}
+              </div>
+              <div className="justify-between flex flex-row">
+                <div className="flex flex-col">
+                  <div className="flex flex-row items-center text-center mb-5">
+                    <div className="flex ">
+                      {Array.from({ length: review.rate }, (_, i) => (
+                        <BlackFullStar key={`full-${i}`} />
+                      ))}
+                      {Array.from({ length: 5 - review.rate }, (_, i) => (
+                        <BlackEmptyStar key={`empty-${i}`} />
+                      ))}
                     </div>
-                    <div className="flex w-16 text-acodegray-500">
-                      향의 세기
-                    </div>
-                    <div className="flex w-16">
-                      {intensityMapping[review.intensity]}
+
+                    <div className="text-acodegray-400 caption2 ml-[10px]">
+                      {maskingNickname(review.nickname)}
                     </div>
                   </div>
-                  <div className="flex flex-row">
-                    <div className="flex w-12 text-acodegray-500">지속성</div>
-                    <div className="flex w-16">
-                      {longevityMapping[review.longevity]}
-                    </div>
-                    <div className="flex w-16 text-acodegray-500">스타일</div>
-                    <div className="flex flex-wrap w-32">
-                      {review.style
-                        .split(', ')
-                        .map((style, styleIndex, array) => (
-                          <span key={style}>
-                            {styleMapping[style]}
-                            {styleIndex < array.length - 1 ? (
+                  <div className="text-[16px] font-semibold leading-[18px] tracking-[-0.4px] text-acodeblack mb-2.5">
+                    {review.comment}
+                  </div>
+                  <div className="mb-5">{review.textReview}</div>
+                  <div className="body2 font-medium grid grid-cols-[95px_128px] gap-x-[42px]">
+                    {Object.entries(keyMapping).map(([key, value], idx) => (
+                      <div className="flex">
+                        <span
+                          className={`${
+                            idx % 2 ? 'w-[63px]' : 'w-12'
+                          } mr-3 text-acodegray-500 shrink-0`}
+                        >
+                          {key}
+                        </span>
+                        <span className="shrink-0 text-acodeblack">
+                          {engToKor(value, review[value])}
+                        </span>
+                      </div>
+                    ))}
+                    <div className="flex">
+                      <span className="w-[63px] mr-3 text-acodegray-500 shrink-0">
+                        스타일
+                      </span>
+                      <div className="text-acodeblack shrink-0 flex flex-wrap w-[128px]">
+                        {review.style.map((style: string, idx: number) => (
+                          <span key={style} className="">
+                            <span className="">{styleMapping[style]}</span>
+                            {idx < review.style.length - 1 && (
                               <span className="text-acodegray-200 mx-1">|</span>
-                            ) : (
-                              ''
                             )}
                           </span>
                         ))}
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
+              {index !== data.reviewInfoList.length - 1 && (
+                <hr className="my-11 border-t-[2px] border-acodegray-50" />
+              )}
             </div>
-            {index !== data.reviewInfoList.length - 1 && (
-              <hr className="my-11 border-t-[2px] border-acodegray-50" />
-            )}
-          </div>
-        );
-      })}
+          );
+        },
+      )}
     </div>
   );
 };
