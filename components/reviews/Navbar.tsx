1c153cae26145373956a22804a674804a5de51fc
DaegyunOh <oooppq@naver.com>
2024-01-27 14:10:18 +0900

    feat: 리뷰작성 X 버튼 라우팅 적용

--- components/reviews/Navbar.tsx
+++ components/reviews/Navbar.tsx
@@ -1,15 +1,19 @@
+'use client';
+
 import { XIcon } from '@/public/images';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
     <div className="flex fixed top-0 items-center w-full max-w-[430px] pt-[13px] pl-[10px] pr-[13px] pb-[11px] bg-acodewhite z-20">
       <h3 className="h1 h-[30px] absolute top-[13px] left-1/2 -translate-x-2/4 text-acodeblack">
         리뷰
       </h3>
-      <Link href="/" className="ml-auto">
+      <button type="button" className="ml-auto" onClick={() => router.back()}>
         <XIcon className=" fill-acodeblack" />
-      </Link>
+      </button>
     </div>
   );
 };
