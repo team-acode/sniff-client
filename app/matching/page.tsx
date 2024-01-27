68825f11e253a1f7170fbaad951969c2653d74bf
Jiseok <chae0_@naver.com>
2024-01-27 17:17:23 +0900

    feature: matching 기본화면 공백추가

--- app/matching/page.tsx
+++ app/matching/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '@/components/matchingtest/Header';
 import Test from '@/components/matchingtest/Test';
+import More from '@/components/matchingtest/More';
 
 const Page = () => {
   return (
     <div className="mx-3">
       <Header />
       <Test />
+      <More />
     </div>
   );
 };
---
+++ components/matchingtest/More.tsx
@@ -0,0 +1,8 @@
+const More = () => {
+  return (
+    <div className="mt-[35px] bg-acodegray-50 border border-acodegray-50 h-[123px] text-acodegray-200 flex justify-center items-center">
+      <div>추후 추가예정이에요</div>
+    </div>
+  );
+};
+export default More;
