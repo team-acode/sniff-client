156d02790f8a527e4ad63924a75df36bf3b6ca8a
DaegyunOh <oooppq@naver.com>
2024-01-27 13:34:25 +0900

    style: 리뷰 작성 전체적인 스타일 사항 적용

--- components/reviews/Modal.tsx
+++ components/reviews/Modal.tsx
@@ -60,32 +60,59 @@ function Modal({ onReturn }: ModalProps) {
           스타일
         </div>
         <div className="flex w-full overflow-x-auto">
-          <div className="flex whitespace-nowrap">
+          <div className="flex whitespace-nowrap h-[30px]">
             {selectedOptions.map((option) => (
-              <div key={option} className="flex items-center mr-1">
-                <span className="px-2.5 py-2 body2 text-white bg-black rounded-full border flex items-center justify-center">
-                  {option}
-                  <button
-                    type="button"
-                    className="ml-1"
-                    onClick={() => removeOption(option)}
+              <div
+                key={option}
+                className="body2 text-acodewhite bg-black border flex items-center justify-center rounded-[50px] mr-1 px-[10px]"
+              >
+                <span className="">{option}</span>
+                <button
+                  type="button"
+                  className="ml-[2px]"
+                  onClick={() => removeOption(option)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="15"
+                    height="16"
+                    viewBox="0 0 15 16"
+                    fill="none"
                   >
-                    X
-                  </button>
-                </span>
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M3.52451 3.13867L2.64062 4.02256L6.61829 8.00022L2.6409 11.9776L3.52479 12.8615L7.50217 8.8841L11.4795 12.8614L12.3633 11.9775L8.38605 8.00022L12.3636 4.02265L11.4797 3.13877L7.50217 7.11633L3.52451 3.13867Z"
+                      fill="white"
+                    />
+                  </svg>
+                </button>
               </div>
             ))}
           </div>
+          {}
           {selectedOptions.length < 3 && (
             <button
               type="button"
               onClick={handleModalOpen}
-              className="px-2.5 py-1 body2 h-[30px] text-acodegray-400 rounded-full border flex items-center justify-center"
+              className="h-[30px] body2 text-acodegray-400 rounded-full border flex items-center justify-center"
             >
-              <span className="w-6 h-6 text-acodeblack mr-[2px] text-[20px]">
-                +
-              </span>
-              {selectedOptions.length === 0 && '어떤 스타일과 어울릴까요?'}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                className="mx-[3px]"
+              >
+                <path
+                  d="M11.2857 11.2857V7H12.7143V11.2857H17V12.7143H12.7143V17H11.2857V12.7143H7V11.2857H11.2857Z"
+                  fill="#292323"
+                />
+              </svg>
+              {selectedOptions.length === 0 && (
+                <span className="mr-[10px]">어떤 스타일과 어울릴까요?</span>
+              )}
             </button>
           )}
         </div>
