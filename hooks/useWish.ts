687f1e4916465216891d16ba3ce77e48660e9ff7
DaegyunOh <oooppq@naver.com>
2024-01-29 22:12:45 +0900

    hotfix: cors 에러 해결 복구 및 보충

--- hooks/useWish.ts
+++ hooks/useWish.ts
@@ -1,13 +1,18 @@
 import { useSession } from '@/hooks/useSession';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 export const useWish = (id: number, initialState: boolean) => {
   const [isWishOn, setIsWishOn] = useState<boolean>(initialState);
   const userInfo = useSession();
+  const router = useRouter();
 
   const handleClickWish = async (e: React.MouseEvent) => {
     e.preventDefault();
-    if (!userInfo) return;
+    if (!userInfo) {
+      router.push('/login');
+      return;
+    }
 
     const res = await fetch(`/auth/fragrance/${id}/scrap`, {
       method: 'POST',
