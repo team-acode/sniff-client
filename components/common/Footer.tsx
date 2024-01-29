72835440411eee071e1f94604444eb61844746fb
DaegyunOh <oooppq@naver.com>
2024-01-30 00:00:23 +0900

    style footer QA 적용

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -4,19 +4,29 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <div className="pt-[25px] pb-10 bg-acodegray-50 pl-4">
-      <Link href="/" className="caption1 font-medium block mb-2.5 ml-[1px]">
-        향수제품추가요청
+      <Link
+        href="https://docs.google.com/forms/d/e/1FAIpQLScViRETa4DlBZZ6HQKKD0PPlEj3zfdYjPaEPS9bs3nbHTOJgQ/viewform?usp=sharing"
+        className="caption1 font-medium block mb-2.5 ml-[1px]"
+        target="_blank"
+      >
+        향수 제품 추가 요청
       </Link>
-      <Link href="/" className="caption1 font-medium block mb-[38px] ml-[1px]">
-        광고제휴문의
+      <Link
+        href="https://docs.google.com/forms/d/e/1FAIpQLScdUKIiEcWXK7DAn7fmf9HxAG4epWDRTfTfQg6EQu0JPf5ScA/viewform?usp=sharing"
+        className="caption1 font-medium block mb-[38px] ml-[1px]"
+        target="_blank"
+      >
+        광고 제휴 및 입점 문의
       </Link>
       <div className="flex mr-[15px] mb-[21px] text-[10px] font-medium text-acodegray-400 leading-[15px]">
         <AcodeLogoGray className="mr-auto" />
-        <Link href="/" className="mr-5 mt-1">
-          이용약관
-        </Link>
-        <Link href="/" className="mt-1">
-          개인정보처리
+        <Link
+          href="https://www.notion.so/b64e9e00aa364583ad9906080210f853?pvs=4"
+          className="mt-1"
+          target="_blank"
+        >
+          <span className="mr-5">이용약관</span>
+          <span className="">개인정보 처리 방침</span>
         </Link>
       </div>
       <div className="text-[10px] font-medium text-acodegray-400 leading-[15px] grid grid-cols-[61px_188px] gap-x-[17px] mb-[25px]">
