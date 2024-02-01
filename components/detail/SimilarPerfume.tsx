2ca2f0424b8714301ab187b85cf03cbf864dfb6c
DaegyunOh <oooppq@naver.com>
2024-02-01 18:55:39 +0900

    hotfix: 유사 향수 정보 가려지는 문제 수정

--- components/detail/SimilarPerfume.tsx
+++ components/detail/SimilarPerfume.tsx
@@ -50,15 +50,16 @@ const SimilarPerfume = async ({ id }: SimilarProps) => {
           <Link
             href={`/perfumes/${perfume.fragranceId}`}
             key={perfume.fragranceName}
-            className="relative w-[138px] h-[138px] shrink-0"
+            className="relative w-[138px] shrink-0"
           >
             <Image
               // placeholder="blur"
               // blurDataURL={blurImageUrls[index]}
               src={perfume.thumbnail}
               alt={`Test Perfume ${index + 1}`}
-              fill
-              className="w-[138px] h-[138px] rounded-[4px] object-cover"
+              width={138}
+              height={138}
+              className="w-[138px] object-cover rounded-[4px]"
             />
             <div className="w-[134px] h-[35px] flex flex-col justify-center bg-white pl-[2px] mt-[10px]">
               <div className="text-acodegray-500 caption2 mb-1">
