48d049fb3f768b1233852d27836e022838bc09ea
DaegyunOh <oooppq@naver.com>
2024-02-01 22:02:48 +0900

    hotfix: 구매처 하나만 보여지도록 수정

--- components/detail/HereToBuy.tsx
+++ components/detail/HereToBuy.tsx
@@ -29,40 +29,42 @@ const HereTobuy = async ({ id }: BuyProps) => {
       <div className="mx-4">
         <div className="h2 mb-[30px]">여기서 구매할 수 있어요</div>
         <div className="grid gap-y-3">
-          {data.purchaseList?.map(
-            ({
-              title,
-              link,
-              image,
-            }: {
-              title: string;
-              link: string;
-              image: string;
-            }) => (
-              <Link key={`${link}-${image}`} href={link} target="_blank">
-                <div className="flex items-center">
-                  <Image
-                    src={image}
-                    alt="vendor"
-                    width={80}
-                    height={80}
-                    objectFit="cover"
-                    className="mr-[14px] w-20 h-20 rounded-sm"
-                  />
+          {data.purchaseList
+            ?.slice(0, 1)
+            .map(
+              ({
+                title,
+                link,
+                image,
+              }: {
+                title: string;
+                link: string;
+                image: string;
+              }) => (
+                <Link key={`${link}-${image}`} href={link} target="_blank">
+                  <div className="flex items-center">
+                    <Image
+                      src={image}
+                      alt="vendor"
+                      width={80}
+                      height={80}
+                      objectFit="cover"
+                      className="mr-[14px] w-20 h-20 rounded-sm"
+                    />
 
-                  <div className="flex flex-col justify-center body2 font-medium">
-                    <div className="mb-[10px]">{title}</div>
-                    <div className="h-[21px] mb-[2px] text-acodegray-500">
-                      {data.brandName}
-                    </div>
-                    <div className="leading-[14px] tracking-[-0.35px] text-acodegray-700">
-                      {data.fragranceName}
+                    <div className="flex flex-col justify-center body2 font-medium">
+                      <div className="mb-[10px]">{title}</div>
+                      <div className="h-[21px] mb-[2px] text-acodegray-500">
+                        {data.brandName}
+                      </div>
+                      <div className="leading-[14px] tracking-[-0.35px] text-acodegray-700">
+                        {data.fragranceName}
+                      </div>
                     </div>
                   </div>
-                </div>
-              </Link>
-            ),
-          ) || (
+                </Link>
+              ),
+            ) || (
             <div className="body2 h-20 w-full bg-acodegray-50 inline-flex items-center justify-center text-acodegray-300 rounded">
               구매처를 찾는 중이에요
             </div>
